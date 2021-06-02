5f98d3feeaa231d688f99e1eac743a65093a584d
Google Play Team <no-reply@google.com>
2021-06-02 09:33:17 -0700

    Update Target SDK version to 29
    PiperOrigin-RevId: 377077430

--- GooglePlayPlugins/com.google.android.appbundle/Editor/Scripts/Internal/BuildTools/AndroidSdkPlatform.cs
+++ GooglePlayPlugins/com.google.android.appbundle/Editor/Scripts/Internal/BuildTools/AndroidSdkPlatform.cs
@@ -28,8 +28,8 @@ namespace Google.Android.AppBundle.Editor.Internal.BuildTools
     {
         // The minimum Android target SDK version supported by Google Play is described here:
         // https://support.google.com/googleplay/android-developer/answer/113469#targetsdk
-        private const int MinimumVersion = 28;
-        private const int LatestVersion = 29;
+        private const int MinimumVersion = 29;
+        private const int LatestVersion = 30;
 
         private static readonly Regex PlatformVersionRegex = RegexHelper.CreateCompiled(@"^android-(\d+)$");
 
