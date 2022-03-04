0cbebdf4a1287eb3eff49aeca30192da19e4ffda
Google Play Team <no-reply@google.com>
2022-03-04 20:25:32 +0000

    Fix Unity Cloud Build issue #172
    Prior to this change AndroidSdkPlatform.cs would check to see if an Android SDK with version 30 and above was installed and throw an error if it was missing.
    
    Now AndroidSdkPlatform.cs only checks the installed Android SDK if targetAndroidSdk is set to "auto (highest installed)", or a version less than 30.
    
    See issue #172 for context.
    
    PiperOrigin-RevId: 432506826

--- GooglePlayPlugins/com.google.android.appbundle/Editor/Scripts/Internal/BuildTools/AndroidSdkPlatform.cs
+++ GooglePlayPlugins/com.google.android.appbundle/Editor/Scripts/Internal/BuildTools/AndroidSdkPlatform.cs
@@ -47,6 +47,14 @@ namespace Google.Android.AppBundle.Editor.Internal.BuildTools
                 return false;
             }
 
+            // If targetSdkVersion is higher than the MinimumVersion, we skip verifying the newest version,
+            // and instead trust that Unity will install it at build time if it isn't already available.
+            var targetSdkVersion = PlayerSettings.Android.targetSdkVersion;
+            if ((int)targetSdkVersion >= MinimumVersion)
+            {
+                return true;
+            }
+
             string ignoredPath;
             var newestVersion = GetNewestVersionAndPath(out ignoredPath);
             if (newestVersion == null)
@@ -76,8 +84,7 @@ namespace Google.Android.AppBundle.Editor.Internal.BuildTools
                 return false;
             }
 
-            var targetSdkVersion = PlayerSettings.Android.targetSdkVersion;
-            if (targetSdkVersion == AndroidSdkVersions.AndroidApiLevelAuto || (int) targetSdkVersion >= MinimumVersion)
+            if (targetSdkVersion == AndroidSdkVersions.AndroidApiLevelAuto)
             {
                 return true;
             }
@@ -86,7 +93,7 @@ namespace Google.Android.AppBundle.Editor.Internal.BuildTools
                 "The currently selected Android Target API Level is {0}, however version {1} is the minimum "
                 + "required to build for Google Play.\n\nClick \"OK\" to change the Target API Level to "
                 + "\"Automatic (highest installed)\", which is currently {2}.",
-                (int) targetSdkVersion, MinimumVersion, newestVersion);
+                (int)targetSdkVersion, MinimumVersion, newestVersion);
             if (buildToolLogger.DisplayActionableErrorDialog(selectedVersionMessage))
             {
                 PlayerSettings.Android.targetSdkVersion = AndroidSdkVersions.AndroidApiLevelAuto;
